fa7270e137ebad7611a2f98cedf5d5f04e1f5cb9
reddii1 <81165693+reddii1@users.noreply.github.com>
2024-10-03 17:50:37 +0100

    Create adf_sql_shir.tf

---
+++ adf_sql_shir.tf
@@ -0,0 +1,28 @@
+###
+### MySQL stuff
+###
+
+### Azure Data factory powerBI integration
+resource "azurerm_data_factory" "adf" {
+  count = length(local.cpenvprefix[terraform.workspace])
+  name                = "adf-${local.location_prefix}-${local.cpenvprefix[terraform.workspace][count.index]}${terraform.workspace}-${var.pdu}"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg_app.name
+}
+
+
+#replying self hosted integration runtime for data factory 
+ resource "azurerm_data_factory_integration_runtime_self_hosted" "shir" {
+  count = length(local.cpenvprefix[terraform.workspace])
+  name            = "shir-adf-${local.location_prefix}-${local.cpenvprefix[terraform.workspace][count.index]}${terraform.workspace}-${var.pdu}"
+  data_factory_id = azurerm_data_factory.adf.id
+}
+
+
+# connecting replica mysql database to data factory for etl to azure sql
+resource "azurerm_data_factory_linked_service_mysql" "mysql-adf-link" {
+  count = length(local.cpenvprefix[terraform.workspace])
+  name              = "mysql-adf-link-${local.location_prefix}-${local.cpenvprefix[terraform.workspace][count.index]}${terraform.workspace}-${var.pdu}"
+  data_factory_id   = azurerm_data_factory.adf.id
+  connection_string = "Server=${azurerm_mysql_flexible_server.replica[count.index].id};port=3306;username=${azurerm_mysql_flexible_server.replica[count.index].administrator_login};password=${azurerm_mysql_flexible_server.replica[count.index].administrator_password}"
+} 
