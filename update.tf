268ce7397fea1030944eed26500c4a420fe34f16
reddii1 <81165693+reddii1@users.noreply.github.com>
2024-10-08 20:56:44 +0100

    Update update.tf

--- update.tf
+++ update.tf
@@ -1,9 +1,9 @@
-### Azure Data factory powerBI integration
+## Azure Data factory powerBI integration
 resource "azurerm_data_factory" "adf" {
   count = length(local.cpenvprefix[terraform.workspace])
   name                = "adf-${local.location_prefix}-${local.cpenvprefix[terraform.workspace][count.index]}${terraform.workspace}-${var.pdu}"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg_app.name
+  resource_group_name = azurerm_resource_group.rg_analytics[0].name
 }
 
 
@@ -27,56 +27,61 @@ resource "azurerm_network_interface" "shir" {
   count = length(local.cpenvprefix[terraform.workspace])
   name                = "nic-shir-adf-${local.location_prefix}-${local.cpenvprefix[terraform.workspace][count.index]}${terraform.workspace}-${var.pdu}"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg_app.name
+  resource_group_name = azurerm_resource_group.rg_analytics[0].name
   ip_configuration {
     name                          = "shir"
-    subnet_id                     = azurerm_subnet.mysqlfs[count.index].id
+    subnet_id                     = azurerm_subnet.fe02[0].id
     private_ip_address_allocation = "Dynamic"
   }
 
   tags = merge(var.tags, local.tags)
 }
 
-resource "random_password" "vm-shir-adf-pass" {
-  count = length(local.cpenvprefix[terraform.workspace])
+# resource "random_password" "vm-shir-adf-pass" {
+#     length      = 20
+#   min_lower   = 1
+#   min_upper   = 1
+#   min_numeric = 1
+#   min_special = 1
+#   special     = true
+# }
 
-  length           = 36
-  override_special = "-"
+
+// This should be randomised and stored in kv eventually
+variable "admin_password" {
+    type = string
+    default = "To_be_randomised_later12443!"
 }
+
+
 resource "azurerm_windows_virtual_machine" "shir_vm" {
   count = length(local.cpenvprefix[terraform.workspace])
   name                = "vm-shir-adf-${local.location_prefix}-${local.cpenvprefix[terraform.workspace][count.index]}${terraform.workspace}-${var.pdu}"
-  computer_name       = "vm-shir-adf"
-  resource_group_name = azurerm_resource_group.rg_app.name
+  resource_group_name = azurerm_resource_group.rg_analytics[0].name
   location            = var.location
   admin_username = "admin"
-  admin_password = "${random_password.vm-shir-adf-pass[count.index].result}"
-  size                  = "Standard_D2as_v4"
-  patch_assessment_mode = "AutomaticByPlatform"
-  network_interface_ids = [
-    azurerm_network_interface.shir.id,
-  ]
+  admin_password = var.admin_password
+  size                  = "Standard_D2s_v3"
+  # This is from the GoldImagesDevGallery/GoldImagesGallery depending on the environment
+  //source_image_id = data.azurerm_shared_image_version.win2019_latestGoldImage.id
+  source_image_id = "/subscriptions/67e5f2ee-6e0a-49e3-b533-97f0beec351c/resourceGroups/rg-dwp-dev-ss-shared-images/providers/Microsoft.Compute/galleries/GoldImagesDevGallery/images/WIN2019-CIS2/versions/3.051.21819"
+  network_interface_ids = [azurerm_network_interface.shir[0].id,]
   os_disk {
-    name                 = "osd-shir-adf"
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2022-datacenter-azure-edition"
-    version   = "latest"
-  }
-  identity {
-    type = "SystemAssigned"
-  }
+
 }
 
-resource "azurerm_storage_container" "dfa-shir" {
-  name                  = "shir-install-script"
-  storage_account_name  = azurerm_storage_account.omilia.name
-  container_access_type = "private"
-}
-locals {
-  script_name = ""
-}
+
+# resource "azurerm_storage_container" "dfa-shir" {
+#   name                  = "shir-install-script"
+#   storage_account_name  = azurerm_storage_account.omilia.name
+#   container_access_type = "private"
+# }
+# locals {
+#   script_name = ""
+# }
+
+# // to do: Create script to put shir on vm
+# //        create az sql db for curated data
